c9df324a42b7d5a716797c2af3d92a84947f6b13
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-07-14 15:59:00 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,8 +132,8 @@ bot.on("message", async message => {
             return message.reply("Sorry, you don't have permissions to use this!");
         var splitMessage = message.content.split(" ");
         var deleteCount = parseInt(splitMessage[1], 10)
-        if (!deleteCount || deleteCount < 2 || deleteCount > 500)
-            return message.reply("Please provide a number between 2 and 500 for the number of messages to delete. ");
+        if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+            return message.reply("Please provide a number between 2 and 100 for the number of messages to delete. ");
         var fetched = await message.channel.fetchMessages({
             limit: deleteCount
         });
