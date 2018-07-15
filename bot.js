7efd21d025d4da37cd89438c5e4baeadebeb82c1
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-07-15 16:55:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -282,7 +282,7 @@ bot.on("message", async message => {
         message.channel.send("I am a bot created by MVPShon for many various uses. My prefix is `u!` Currently, there aren't many things I can do but I keep growing and expanding each day. For the moment, type `u!commands` or `u!admin` to see what I can currently do.");
     }
     if (message.content.startsWith(prefix + "commands")) {
-        message.author.send("My current commands are: \n`urbandictionary` or `ud` - Defines a word as according to Urban Dictionary.\n`google` - Searches Google for your terms. Aliases `g2, search`. Usage: `u!google cat`\n`dog` - Shows a random image of a dog! Aliases (`pupper, doge, puppy`)\n`cat` - Shows a random image of a cat! Aliases (`kitty, neko`)\n`roast` - Insult your friends with my ever-growing list of roasts and insults!\n`copy` - Straight forward command. I copy whatever you tell me to. This command works better if I can delete other people's messages.\n`pokedex` or `dex` - Brings up a Pokemon's stats. Putting the Pokemon's name in lowercase will also show a picture!");
+        message.author.send("My current commands are: \n`yt` or `youtube` - Brings up a link to a video based on your terms.\n`urbandictionary` or `ud` - Defines a word as according to Urban Dictionary.\n`google` - Searches Google for your terms. Aliases `g2, search`. Usage: `u!google cat`\n`dog` - Shows a random image of a dog! Aliases (`pupper, doge, puppy`)\n`cat` - Shows a random image of a cat! Aliases (`kitty, neko`)\n`roast` - Insult your friends with my ever-growing list of roasts and insults!\n`copy` - Straight forward command. I copy whatever you tell me to. This command works better if I can delete other people's messages.\n`pokedex` or `dex` - Brings up a Pokemon's stats. Putting the Pokemon's name in lowercase will also show a picture!");
         message.author.send("NSFW Commands: \n`rule34` or `r34` - Shows a hentai image based on your search terms. Usage: `u!rule34 slime`\n`danbooru` or `db` - Shows a hentai image based on your search terms. Usage: `u!danbooru slime`");
     }
     if (message.content.startsWith(prefix + "admin")) {
@@ -464,6 +464,7 @@ bot.on("message", async message => {
                 .addField("SP DEF:", base_stats.sp_def, true)
                 .addField("SPEED:", base_stats.speed, true)
                 .setImage(`https://play.pokemonshowdown.com/sprites/xyani/${args[0]}.gif`, true)
+                .setThumbnail(`https://play.pokemonshowdown.com/sprites/xyani-shiny/${args[0]}.gif`, true)
             message.channel.send(embed)
         });
     }
