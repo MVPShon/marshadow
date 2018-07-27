cbe85de39506a3c6050a340834e04b35225db44e
MVPShon <36894232+MVPShon@users.noreply.github.com>
2018-07-27 07:21:27 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -89,7 +89,28 @@ bot.on("message", async message => {
             });
 
     }
-
+    
+if (message.content.startsWith(prefix + "weather")) {
+    var weather = require('weather-js'); 
+    weather.find({search: `${args.join(" ")}`, degreeType: 'F'}, function(err, result) {
+      if(err) return message.reply('Please enter a valid location!');
+      if (result === undefined || result.length === 0) {
+        message.reply('Please enter a valid location!')
+        return;
+    }
+    var current = result[0].current;
+    var location = result[0].location;
+      let Embed = new Discord.RichEmbed()
+      .setTitle("Weather results for `" + location.name + "`.")
+      .setColor(0xFE2E2E)
+      .addField("Skies Are", current.skytext, true)
+      .addField("Humidity", current.humidity, true)
+      .addField("Currently", current.temperature + "°F", true)
+      .addField("Feels Like", current.feelslike + "°F", true)
+      .setThumbnail(current.imageUrl)
+      message.channel.send(Embed)
+    });
+}
     if (message.content.startsWith(prefix + "urbandictionary") || (message.content.startsWith(prefix + "ud"))) {
         // var webdict = require('webdict');
         // webdict('urbandictionary', `${args.join(" ")}`)
